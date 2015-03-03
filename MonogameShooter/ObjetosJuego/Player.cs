4233b5aaefd72bcdcb8b202ab267578ea4bfd9b1
victorvergel@gregoriofer.com <victorvergel@gregoriofer.com@28dff8f7-4275-7dfd-fbf5-c67612de4570>
2015-03-03 00:35:23 +0000

    Se añade una nueva clase Animacion con la que se permite añadir una animación sobre la nave del jugador cambiando de Sprite de manera automatizada.

--- MonogameShooter/ObjetosJuego/Player.cs
+++ MonogameShooter/ObjetosJuego/Player.cs
@@ -10,7 +10,9 @@ namespace MonogameShooter.ObjetosJuego
     class Player
     {
         // Animation representing the player. Imagen de la nave
-        private Texture2D _playerTexture;
+        //private Texture2D _playerTexture;
+        //sustituimos la imagen normal por una imagen que simula una animación
+        public Animacion _jugadorAnimacion;
         // Position of the Player relative to the upper left side of the screen
         public Vector2 Position;
         // State of the player
@@ -20,20 +22,23 @@ namespace MonogameShooter.ObjetosJuego
         //Get the width of the player ship
         public int Width
         {
-            get { return _playerTexture.Width; }
+            get { return _jugadorAnimacion.FrameWidth; }
         }
-        // Get the height of the player ship
         public int Height
         {
-            get { return _playerTexture.Height; }
+            get { return _jugadorAnimacion.FrameHeight; }
         }
 
 
 
-        public void Initialize(Texture2D texture, Vector2 position)
+
+        //public void Initialize(Texture2D texture, Vector2 position)
+        public void Initialize(Animacion animation, Vector2 position)
         {
 
-            _playerTexture = texture;
+            //     _playerTexture = texture;
+            _jugadorAnimacion = animation;
+
             // Set the starting position of the player around the middle of the screen and to the back
             Position = position;
             // Set the player to be active
@@ -41,13 +46,16 @@ namespace MonogameShooter.ObjetosJuego
             // Set the player health
             _health = 100;
         }
-        public void Update()
+        public void Update(GameTime gameTime)
         {
+            _jugadorAnimacion.Position = Position;
+            _jugadorAnimacion.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw (_playerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f,SpriteEffects.None, 0f);
+            //spriteBatch.Draw(_playerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            _jugadorAnimacion.Draw(spriteBatch);
 
         }
     }
